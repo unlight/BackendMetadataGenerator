5847649ae878575f3f83fe18c75ca89576f33bc8
Roman <s91630277@gmail.com>
2016-02-28 00:19:30 +0300

    Removed old files

--- BackendMetadataGenerator/TypeInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BackendMetadataGenerator
-{
-	public class TypeInfo
-	{
-		public string XPath { get; set; }
-		public bool IsArray { get; set; }
-		public string ClassName { get; set; }
-	}
-}
